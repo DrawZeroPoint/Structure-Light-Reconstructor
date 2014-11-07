632d774e948851968cde7a6fc07a258fc76394b4
DZP <damoninhit@qq.com>
2014-11-07 13:51:24 +0800

    Translate
    English and Chinese were added.

---
+++ Duke/en.ts
@@ -0,0 +1,363 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <location filename="mainwindow.ui" line="338"/>
+        <location filename="mainwindow.ui" line="1734"/>
+        <location filename="mainwindow.ui" line="1737"/>
+        <source>Calibrate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Capture images as displayed:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="194"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;/12 images have been captured.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <source>Illustration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="325"/>
+        <location filename="mainwindow.ui" line="986"/>
+        <location filename="mainwindow.ui" line="1555"/>
+        <source>Capture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="330"/>
+        <source>Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="347"/>
+        <location filename="mainwindow.ui" line="1756"/>
+        <location filename="mainwindow.ui" line="1762"/>
+        <source>Scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="352"/>
+        <location filename="mainwindow.ui" line="1781"/>
+        <location filename="mainwindow.ui" line="1784"/>
+        <source>Reconstruct</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="661"/>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="674"/>
+        <source>Customize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="685"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="708"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="765"/>
+        <source>Left Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="883"/>
+        <location filename="mainwindow.ui" line="1299"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1050"/>
+        <location filename="mainwindow.ui" line="1363"/>
+        <source>Camera Adjust</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1110"/>
+        <location filename="mainwindow.ui" line="1414"/>
+        <source>Snap Speed(ms)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1129"/>
+        <location filename="mainwindow.ui" line="1430"/>
+        <source>Exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1200"/>
+        <source>Right Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1609"/>
+        <source>New Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1612"/>
+        <source>  New  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1615"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Create a new project&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1618"/>
+        <source>Create a new folder to contain project.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1628"/>
+        <source>Ctrl+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1640"/>
+        <source>Open Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1643"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open project&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1653"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1662"/>
+        <location filename="mainwindow.ui" line="1668"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1665"/>
+        <source>  Exit  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1671"/>
+        <source>Close the program.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1681"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1690"/>
+        <source>Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1693"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open cameras&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1696"/>
+        <source>Open Cameras.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1715"/>
+        <source>Projector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1718"/>
+        <source>Turn on/off the projector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1740"/>
+        <source>Active calibration.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1759"/>
+        <source>  Scan  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1765"/>
+        <source>Start scan process.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1787"/>
+        <source>Start reconstruction.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1803"/>
+        <location filename="mainwindow.ui" line="1806"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1809"/>
+        <source>Set the parameters of the project.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1825"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1841"/>
+        <source>简体中文</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="79"/>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>Open Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Calibration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Calibration Actived!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Set</name>
+    <message>
+        <location filename="set.cpp" line="29"/>
+        <source>Grid Width(mm);</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="32"/>
+        <location filename="set.cpp" line="59"/>
+        <source>Grid Height(mm):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="43"/>
+        <source>Calibration Board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="56"/>
+        <source>Grid Width(mm):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="124"/>
+        <source>Projector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="133"/>
+        <source>Black Threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="141"/>
+        <source>White Threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="154"/>
+        <source>Auto Contrast</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="158"/>
+        <source>Save Auto Contrast Images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="161"/>
+        <source>Ray Sampling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="165"/>
+        <source>Reconstruction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="173"/>
+        <source>Export Obj</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="179"/>
+        <source>Export Ply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="185"/>
+        <source>Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="187"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="190"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="210"/>
+        <location filename="set.cpp" line="212"/>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="210"/>
+        <source>Successed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="212"/>
+        <source>Failed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ Duke/zh.ts
@@ -0,0 +1,363 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>主窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <location filename="mainwindow.ui" line="338"/>
+        <location filename="mainwindow.ui" line="1734"/>
+        <location filename="mainwindow.ui" line="1737"/>
+        <source>Calibrate</source>
+        <translation>标定</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Capture images as displayed:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>根据图示拍摄图片</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="194"/>
+        <source>0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;/12 images have been captured.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>/12组图片已被拍摄</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <source>Illustration</source>
+        <translation>图示</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="325"/>
+        <location filename="mainwindow.ui" line="986"/>
+        <location filename="mainwindow.ui" line="1555"/>
+        <source>Capture</source>
+        <translation>获取图像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="330"/>
+        <source>Redo</source>
+        <translation>重新获取</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="347"/>
+        <location filename="mainwindow.ui" line="1756"/>
+        <location filename="mainwindow.ui" line="1762"/>
+        <source>Scan</source>
+        <translation>扫描</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="352"/>
+        <location filename="mainwindow.ui" line="1781"/>
+        <location filename="mainwindow.ui" line="1784"/>
+        <source>Reconstruct</source>
+        <translation>重建</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="661"/>
+        <source>Project</source>
+        <translation>工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="674"/>
+        <source>Customize</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="685"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="708"/>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="765"/>
+        <source>Left Camera</source>
+        <translation>左相机</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="883"/>
+        <location filename="mainwindow.ui" line="1299"/>
+        <source>View</source>
+        <translation>实时显示</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1050"/>
+        <location filename="mainwindow.ui" line="1363"/>
+        <source>Camera Adjust</source>
+        <translation>相机调整</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1110"/>
+        <location filename="mainwindow.ui" line="1414"/>
+        <source>Snap Speed(ms)</source>
+        <translation>采集速度(毫秒)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1129"/>
+        <location filename="mainwindow.ui" line="1430"/>
+        <source>Exposure</source>
+        <translation>曝光补偿</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1200"/>
+        <source>Right Camera</source>
+        <translation>右相机</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1609"/>
+        <source>New Project</source>
+        <translation>新建工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1612"/>
+        <source>  New  </source>
+        <translation>新建工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1615"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Create a new project&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>新建一个工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1618"/>
+        <source>Create a new folder to contain project.</source>
+        <translation>创建一个文件夹作为工程文件夹.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1628"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1640"/>
+        <source>Open Project</source>
+        <translation>打开工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1643"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open project&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>打开工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1653"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1662"/>
+        <location filename="mainwindow.ui" line="1668"/>
+        <source>Exit</source>
+        <translation>退出程序</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1665"/>
+        <source>  Exit  </source>
+        <translation>退出程序</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1671"/>
+        <source>Close the program.</source>
+        <translation>退出程序.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1681"/>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1690"/>
+        <source>Camera</source>
+        <translation>打开相机</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1693"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open cameras&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>打开相机</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1696"/>
+        <source>Open Cameras.</source>
+        <translation>打开相机.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1715"/>
+        <source>Projector</source>
+        <translation>投影仪</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1718"/>
+        <source>Turn on/off the projector</source>
+        <translation>打开/关闭投影仪</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1740"/>
+        <source>Active calibration.</source>
+        <translation>启动标定.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1759"/>
+        <source>  Scan  </source>
+        <translation>扫描</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1765"/>
+        <source>Start scan process.</source>
+        <translation>开始扫描.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1787"/>
+        <source>Start reconstruction.</source>
+        <translation>开始重建.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1803"/>
+        <location filename="mainwindow.ui" line="1806"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1809"/>
+        <source>Set the parameters of the project.</source>
+        <translation>设置工程参数.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1825"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1841"/>
+        <source>简体中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="79"/>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>Open Directory</source>
+        <translation>打开文件夹</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Calibration</source>
+        <translation>标定</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Calibration Actived!</source>
+        <translation>标定功能启动!</translation>
+    </message>
+</context>
+<context>
+    <name>Set</name>
+    <message>
+        <location filename="set.cpp" line="29"/>
+        <source>Grid Width(mm);</source>
+        <translation>网格宽度(mm)</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="32"/>
+        <location filename="set.cpp" line="59"/>
+        <source>Grid Height(mm):</source>
+        <translation>网格高度(mm):</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="43"/>
+        <source>Calibration Board</source>
+        <translation>标定板</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="56"/>
+        <source>Grid Width(mm):</source>
+        <translation>网格宽度(mm):</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="124"/>
+        <source>Projector</source>
+        <translation>投影仪</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="133"/>
+        <source>Black Threshold</source>
+        <translation>黑色阈值</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="141"/>
+        <source>White Threshold</source>
+        <translation>白色阈值</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="154"/>
+        <source>Auto Contrast</source>
+        <translation>自动对比度</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="158"/>
+        <source>Save Auto Contrast Images</source>
+        <translation>保存自动对比度图像</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="161"/>
+        <source>Ray Sampling</source>
+        <translation>射线采样</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="165"/>
+        <source>Reconstruction</source>
+        <translation>重建</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="173"/>
+        <source>Export Obj</source>
+        <translation>输出Obj格式</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="179"/>
+        <source>Export Ply</source>
+        <translation>输出Ply格式</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="185"/>
+        <source>Export</source>
+        <translation>输出</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="187"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="190"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="210"/>
+        <location filename="set.cpp" line="212"/>
+        <source>Test</source>
+        <translation>测试</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="210"/>
+        <source>Successed!</source>
+        <translation>成功!</translation>
+    </message>
+    <message>
+        <location filename="set.cpp" line="212"/>
+        <source>Failed!</source>
+        <translation>失败!</translation>
+    </message>
+</context>
+</TS>
